34372a76fe262d257291a73618c54ca697cd8073
TimurAfonin <rubin316831@gmail.com>
2023-05-20 18:43:11 +0700

    Adding game.h

---
+++ test/test_game.h
@@ -0,0 +1,10 @@
+#ifndef TEST_GAME_H
+#define TEST_GAME_H
+
+void test_displayMatches();
+void test_getPlayerChoice();
+void test_switchPlayerTurn();
+void test_announceWinner();
+void test_matches();
+
+#endif
