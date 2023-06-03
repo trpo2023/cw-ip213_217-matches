20889c630e1ba417ffa30ccf9558e9d7e933445e
Arkady <pozdeev2003.arkasha@gmail.com>
2023-06-03 21:05:37 +0700

    add ctest.h in test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,31 +1,31 @@
-#include <string>
-#include <vector>
-#include <"../src/game.h"
+#include <iostream>
+#include "../src/game.h"
+#include "../thirdparty/ctest.h"
 
-CTEST(getPlayerChoiceTest, InvalidChoiceLessThan1) {
+CTEST(int getPlayerChoiceTest,int InvalidChoiceLessThan1) {
 int turn = 1;
 int count = 5;
 int choice = getPlayerChoice(turn, count);
-EXPECT_EQ(choice, getPlayerChoice(turn, count));
+ASSERT_TRUE(choice < 1);
 }
 
-CTEST(getPlayerChoiceTest, InvalidChoiceGreaterThan10) {
+CTEST(int getPlayerChoiceTest,int InvalidChoiceGreaterThan10) {
 int turn = 1;
 int count = 5;
 int choice = getPlayerChoice(turn, count);
-EXPECT_EQ(choice, getPlayerChoice(turn, count));
+ASSERT_TRUE(choice > 10);
 }
 
-CTEST(getPlayerChoiceTest, InvalidCountLessThanChoice) {
-int turn = 1;
-int count = 5;
+CTEST(int getPlayerChoiceTest,int InvalidCountLessThanChoice) {
+int turn = 3;
+int count = 2;
 int choice = getPlayerChoice(turn, count);
-EXPECT_EQ(choice, getPlayerChoice(turn, count));
+ASSERT_TRUE(choice == 0);
 }
 
-CTEST(getPlayerChoiceTest, ValidChoice) {
+CTEST(int getPlayerChoiceTest,int ValidChoice) {
 int turn = 1;
 int count = 5;
 int choice = getPlayerChoice(turn, count);
-EXPECT_EQ(choice, getPlayerChoice(turn, count));
+ASSERT_TRUE(choice >= 1 && choice <= 10 && choice <= count);
 }
